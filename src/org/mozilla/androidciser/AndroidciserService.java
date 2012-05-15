d48c9f928528d1194c000cb10eb1781cf90dd3e9
Eitan Isaacson <eitan@monotonous.org>
2012-05-15 14:31:57 -0700

    Add ICS event types

--- src/org/mozilla/androidciser/AndroidciserService.java
+++ src/org/mozilla/androidciser/AndroidciserService.java
@@ -94,20 +94,34 @@ public class AndroidciserService extends AccessibilityService {
     /* AccessibilityEvent.eventTypeToString in only in API version 14 */
     private static String eventTypeToString(int eventType) {
         switch (eventType) {
-        case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
-            return "TYPE_NOTIFICATION_STATE_CHANGED";
         case AccessibilityEvent.TYPE_VIEW_CLICKED:
             return "TYPE_VIEW_CLICKED";
-        case AccessibilityEvent.TYPE_VIEW_FOCUSED:
-            return "TYPE_VIEW_FOCUSED";
         case AccessibilityEvent.TYPE_VIEW_LONG_CLICKED:
             return "TYPE_VIEW_LONG_CLICKED";
         case AccessibilityEvent.TYPE_VIEW_SELECTED:
             return "TYPE_VIEW_SELECTED";
+        case AccessibilityEvent.TYPE_VIEW_FOCUSED:
+            return "TYPE_VIEW_FOCUSED";
         case AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED:
             return "TYPE_VIEW_TEXT_CHANGED";
         case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
-            return "TYPE_VIEW_WINDOW_STATE_CHANGED";
+            return "TYPE_WINDOW_STATE_CHANGED";
+        case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
+            return "TYPE_NOTIFICATION_STATE_CHANGED";
+        case AccessibilityEvent.TYPE_VIEW_HOVER_ENTER:
+            return "TYPE_VIEW_HOVER_ENTER";
+        case AccessibilityEvent.TYPE_VIEW_HOVER_EXIT:
+            return "TYPE_VIEW_HOVER_EXIT";
+        case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_START:
+            return "TYPE_TOUCH_EXPLORATION_GESTURE_START";
+        case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_END:
+            return "TYPE_TOUCH_EXPLORATION_GESTURE_END";
+        case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
+            return "TYPE_WINDOW_CONTENT_CHANGED";
+        case AccessibilityEvent.TYPE_VIEW_SCROLLED:
+            return "TYPE_VIEW_SCROLLED";
+        case AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED:
+            return "TYPE_VIEW_TEXT_SELECTION_CHANGED";
         default:
             return "TYPE_UNKNOWN";
         }
